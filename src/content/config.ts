f8f4bfa0e43b17c0086974a942cd2445e57ce8f4
Akihiro Tojo <akihiro.tj.biz+github@gmail.com>
2024-08-11 19:00:12 +0900

    fixup! Add content collections of article and tag

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,7 @@ const article = defineCollection({
 });
 
 const tag = defineCollection({
-	type: "data",
+	type: "content",
 	schema: tagFrontmatterSchema,
 });
 
--- src/content/schemas.ts
+++ src/content/schemas.ts
@@ -4,12 +4,11 @@ import { z } from "astro:content";
 export const articleFrontmatterSchema = z.object({
 	title: z.string(),
 	description: z.string().optional(),
-	publishedDate: z.string(),
-	updatedDate: z.string().optional(),
+	publishedDate: z.date(),
+	updatedDate: z.date().optional(),
 	tags: z.array(z.string()).optional(),
 });
 
 export const tagFrontmatterSchema = z.object({
-	id: z.string(),
 	name: z.string(),
 });
