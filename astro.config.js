a3cdf98f2e5aeef4b12d599fbb9d4c15a44152b9
Akihiro Tojo <akihiro.tj.biz+github@gmail.com>
2024-08-11 16:00:42 +0900

    Setup biome (#4)
    * Move some dependencies to devDependencies
    
    * Add biome to project
    
    * Format codes

--- astro.config.js
+++ astro.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  })]
+	server: {
+		port: 3000,
+	},
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 });
