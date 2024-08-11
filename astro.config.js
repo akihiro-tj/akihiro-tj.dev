79ae5ec9288fe19ad806cc787b0ea39dd3c7680d
Akihiro Tojo <akihiro.tj.biz+github@gmail.com>
2024-08-11 15:17:29 +0900

    Setup astro project (#3)
    * Setup astro
    
    * Add react to the project
    
    * Add tailwind to the project
    
    * Add path alias to tsconfig
    
    * Setup shadcn-ui
    
    * Import globals.css
    
    * Update astro tailwind config
    
    to prevent serving the tailwind base styles twice
    
    * Update target extension in tailwind config
    
    * Replace mjs with js
    
    * Replace export statement in tailwind config
    
    * Set local host port number
    
    * Add name to package.json

---
+++ astro.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  })]
+});
