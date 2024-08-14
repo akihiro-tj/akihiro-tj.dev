2471771b1e1793a7cad1f3bf2ae000b7a56f2811
Akihiro Tojo <akihiro.tj.biz+github@gmail.com>
2024-08-14 19:25:53 +0900

    Define rss setting

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,21 @@
+import { SITE_DESCRIPTION, SITE_TITLE } from "@/constants";
+import { ArticleRepositoryImpl } from "@/infrastructure/file-system/article-repository";
+import { TagRepositoryImpl } from "@/infrastructure/file-system/tag-repository";
+import rss from "@astrojs/rss";
+
+export async function GET(context) {
+	const tagRepository = new TagRepositoryImpl();
+	const articleRepository = new ArticleRepositoryImpl(tagRepository);
+	const articles = await articleRepository.getAll();
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site,
+		items: articles.map((article) => ({
+			title: article.title,
+			description: article.description,
+			link: article.url,
+			pubDate: article.publishedDate,
+		})),
+	});
+}
