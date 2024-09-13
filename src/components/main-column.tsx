b4ce4c6416e323bdec0a0ef255c8c1253b9ad511
Akihiro Tojo <akihiro.021109@gmail.com>
2024-09-14 00:29:05 +0900

    Replace margin of `MainColumn` with padding

--- src/components/main-column.tsx
+++ src/components/main-column.tsx
@@ -2,6 +2,6 @@ import type { PropsWithChildren } from "react";
 
 export const MainColumn: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<main className="max-w-3xl mx-auto px-4 my-12 lg:my-14">{children}</main>
+		<main className="max-w-3xl mx-auto px-4 py-12 lg:py-14">{children}</main>
 	);
 };
