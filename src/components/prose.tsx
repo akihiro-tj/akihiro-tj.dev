2cd1b72c790eaec7733318899e2469c3d496cf60
Akihiro Tojo <akihiro.021109@gmail.com>
2024-09-14 00:28:34 +0900

    Update prose width

--- src/components/prose.tsx
+++ src/components/prose.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export const Prose: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<div className="prose lg:prose-lg prose-slate hover:prose-a:no-underline">
+		<div className="w-full prose lg:prose-lg hover:prose-a:no-underline">
 			{children}
 		</div>
 	);
