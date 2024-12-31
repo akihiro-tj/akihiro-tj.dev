5242c54d0132263d77245d4b99dd1227d3d67f69
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2024-12-31 17:31:21 +0900

    Setup biome (#2)
    * Install biome
    
    * Add biome.jsonc
    
    * Add vscode settings
    
    * Add biome script
    
    * Add biome ignore
    
    * Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.akihiro-tj.dev",
-  server: {
+	server: {
 		port: 3000,
 	},
 });
