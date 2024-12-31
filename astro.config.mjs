04b5d3a53d305bf101cb9c4644f2d38f8ba7ae45
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2024-12-31 19:20:05 +0900

    Setup shadcn/ui (#4)
    * Install react
    
    * Install tailwindcss
    
    * Add globals.css
    
    * Import globals.css
    
    * Update astro.config.mjs
    
    * Update tsconfig.json
    
    * Update import path
    
    * Setup shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
-// @ts-check
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -7,4 +8,10 @@ export default defineConfig({
 	server: {
 		port: 3000,
 	},
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 });
