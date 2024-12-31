3962cf02aad09a705cd4ea152719059a4a2b2bc1
Akihiro Tojo <akihiro.021109@gmail.com>
2024-12-31 18:44:04 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,10 @@ export default defineConfig({
 	server: {
 		port: 3000,
 	},
-	integrations: [react(), tailwind()],
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 });
