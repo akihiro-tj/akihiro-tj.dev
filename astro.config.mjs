a4879b04d177524544ef216b94b80c90b53077b7
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2024-12-31 17:01:18 +0900

    Setup astro (#1)
    * Setup astro
    
    * Add app name to packages.json
    
    * Add astro config

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://www.akihiro-tj.dev",
+  server: {
+		port: 3000,
+	},
+});
