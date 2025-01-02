ff4003af515025ea600b11a2dba037a8b623972b
Akihiro Tojo <akihiro.tojo.02+github@gmail.com>
2025-01-02 14:44:37 +0900

    Add query to show only published contents on prod (#30)
    * Add filter to show only published contents on prod
    
    * Update queries in repositories

--- src/repositories/article.ts
+++ src/repositories/article.ts
@@ -2,6 +2,7 @@ import { Article } from "@/entities/article";
 import { createMicroCmsClient } from "@/libs/microcms";
 import { articleSchema } from "@/schemas/article";
 import { microCmsContentsSchema } from "@/schemas/microcms-contents";
+import type { MicroCMSQueries } from "microcms-js-sdk";
 
 export interface IArticleRepository {
 	find(contentId: string): Promise<Article>;
@@ -9,20 +10,33 @@ export interface IArticleRepository {
 	findByTag(tagId: string): Promise<Article[]>;
 }
 
-const commonQueries = {
+const baseQueries: MicroCMSQueries = {
 	fields: "id,publishedAt,updatedAt,showUpdatedAt,title,tags,content",
 	orders: "-publishedAt",
 };
 
+const stagingQueries: MicroCMSQueries = {
+	...baseQueries,
+};
+
+const productionQueries: MicroCMSQueries = {
+	...baseQueries,
+	filters: "isPublished[equals]true",
+};
+
 export class ArticleRepository implements IArticleRepository {
 	private microCmsClient = createMicroCmsClient();
+	private baseQueries =
+		import.meta.env.HOSTING_ENVIRONMENT === "production"
+			? productionQueries
+			: stagingQueries;
 
 	async find(contentId: string): Promise<Article> {
 		const response = await this.microCmsClient.get({
 			endpoint: "article",
 			contentId,
 			queries: {
-				...commonQueries,
+				...this.baseQueries,
 			},
 		});
 		const rawArticle = articleSchema.parse(response);
@@ -34,7 +48,7 @@ export class ArticleRepository implements IArticleRepository {
 		const response = await this.microCmsClient.get({
 			endpoint: "article",
 			queries: {
-				...commonQueries,
+				...this.baseQueries,
 			},
 		});
 		const rawContents = microCmsContentsSchema.parse(response.contents);
@@ -49,8 +63,8 @@ export class ArticleRepository implements IArticleRepository {
 		const response = await this.microCmsClient.get({
 			endpoint: "article",
 			queries: {
-				...commonQueries,
-				filters: `tags[contains]${tagId}`,
+				...this.baseQueries,
+				filters: `${this.baseQueries.filters ? `${this.baseQueries.filters}[and]` : ""}tags[contains]${tagId}`,
 			},
 		});
 		const rawContents = microCmsContentsSchema.parse(response.contents);
